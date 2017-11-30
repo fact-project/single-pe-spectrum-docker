9f7100e88d821b10b1a7b43981246c097eec13e6
Jens Buss <jens.buss@tu-dortmund.de>
2017-11-30 12:34:49 +0100

    add default values for run numbers

--- single_pe_spectrum/spe_spectrum_data.sh
+++ single_pe_spectrum/spe_spectrum_data.sh
@@ -6,6 +6,9 @@ infile=${infile:-"/input_data/20120723_"}
 outpath_spectra=${outpath_spectra:-"/output_data/single_pe_spectra.root"}
 outpath_fit=${outpath_fit:-"/output_data/single_pe_spectra_fit.root"}
 
+first_run=${first_run:-9}
+last_run=${last_run:-$first_run}
+
 ROOT_HIST=0 root -q -b -l extract_singles.C+\(\""$infile\"",$first_run,$last_run,\""$drsfile\"",\""${outpath_spectra}\""\)
 
 ROOT_HIST=0 root -q -b -l fit_spectra.C+\(\""$outpath_spectra\"",\""$outpath_fit\""\)
